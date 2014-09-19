1b7a16b89655acd3e8f04ca200aa95275c698d1b
Mitar <mitar.git@tnode.com>
2014-09-19 04:12:22 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   summary: "Mozilla's HTML5 PDF reader, repackaged for Meteor, client & server",
-  version: '0.8.1003-6',
+  version: '1.0.791-1',
   name: 'mrt:pdf.js',
   git: 'https://github.com/peerlibrary/meteor-pdf.js.git'
 });
 
 Package.on_use(function (api) {
-  api.imply('peerlibrary:pdf.js@0.8.1003-6');
+  api.imply('peerlibrary:pdf.js@1.0.791-1');
 });
