2b5c0a23f3f916ff7b51850814efbe545d7cae5e
Mitar <mitar.git@tnode.com>
2014-09-19 02:00:38 -0700

    Set cMapUrl on the client.

--- client.js
+++ client.js
@@ -3,3 +3,5 @@ window.PDFJS = PDFJS;
 
 // TODO: How to properly find out the location of this? And know the query string value?
 PDFJS.workerSrc = '/packages/pdf.js/pdf.js/src/worker_loader.js';
+PDFJS.cMapUrl = '/packages/pdf.js/pdf.js/external/bcmaps/';
+PDFJS.cMapPacked = true;
